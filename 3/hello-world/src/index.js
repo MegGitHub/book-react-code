314cfad3419ab57b97df9cc9816ab98d09126a57
MegGitHub <75002983+MegGitHub@users.noreply.github.com>
2024-05-29 07:49:17 +0900

    feat: ボタンの追加
    エリア1にボタンを追加

--- 3/hello-world/src/index.js
+++ 3/hello-world/src/index.js
@@ -1,36 +1,8 @@
-// getElementByIdを使ってtitle要素を取得
-const title = document.getElementById("title");
-console.log(title);
-// <h1 id="title">Hello World!</h1>
+// buttonタグの生成
+const buttonEl = document.createElement("button");
+buttonEl.textContent = "ボタン";
 
-// querySelectorを使ってtitle要素を取得
-const title2 = document.querySelector("#title");
-console.log(title2);
-// <h1 id="title">Hello World!</h1>
+// エリア1のdivタグを取得
+const divEl = document.querySelector(".container");
 
-// getElementsByClassNameを使ってcontainer要素を取得
-const container = document.getElementsByClassName("container");
-console.log(container);
-// <div class="container">
-//     <p>エリア1です</p>
-// </div>
-// <div class="container">
-//     <p>エリア2です</p>
-// </div>
-
-// querySelectorを使ってcontainer要素を取得
-const container2 = document.querySelector(".container");
-console.log(container2);
-// <div class="container">
-//     <p>エリア1です</p>
-// </div>
-
-// querySelectorAllを使ってcontainer要素を取得
-const container3 = document.querySelectorAll(".container");
-console.log(container3);
-// <div class="container">
-//     <p>エリア1です</p>
-// </div>
-// <div class="container">
-//     <p>エリア2です</p>
-// </div>
+divEl.appendChild(buttonEl);
