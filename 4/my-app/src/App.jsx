873854e9712d6d90669a0e2a202d012839360647
MegGitHub <75002983+MegGitHub@users.noreply.github.com>
2024-07-04 13:58:21 +0900

    ボタン機能を実装

--- 4/my-app/src/App.jsx
+++ 4/my-app/src/App.jsx
@@ -1,8 +1,14 @@
 export const App = () => {
+    // ボタンを押した時に実行する関数を定義
+    const onClickButton = () => {
+        alert("ボタンを押したよ!");
+    };
+
     return (
-        <div>
+        <>
             <h1>Hello, React!</h1>
             <p>How are you?</p>
-        </div>
+            <button onCrick={onClickButton}>ボタン</button>
+        </>
     );
 };
