410a036be8e37f3c1b173c78c1d694cfadb8442d
MegGitHub <75002983+MegGitHub@users.noreply.github.com>
2024-07-04 14:49:23 +0900

    useStateでカウントアップを実装

--- 4/my-app/src/App.jsx
+++ 4/my-app/src/App.jsx
@@ -1,9 +1,13 @@
 import { ColoredMessage } from "./components/ColorMessage";
+import { useState } from "react";
 
 export const App = () => {
+    // Stateの定義
+    const [num, setNum] = useState(0);
+
     // ボタンを押した時に実行する関数を定義
     const onClickButton = () => {
-        alert("ボタンを押したよ!");
+        setNum((prev) => prev + 1);
     };
 
     return (
@@ -11,7 +15,8 @@ export const App = () => {
             <h1 style={{ color: "red" }}>Hello, React!</h1>
             <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
             <ColoredMessage color="pink">元気です!</ColoredMessage>
-            <button onCrick={onClickButton}>button</button>
+            <button onClick={onClickButton}>button</button>
+            <p>{num}</p>
         </>
     );
 };
